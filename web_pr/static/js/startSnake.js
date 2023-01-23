3e79aedd00001035ebacd85acb7235ef5247035d
JennyD95 <117718267+JennyD95@users.noreply.github.com>
2023-01-23 11:39:55 +0100

    Update startSnake.js

--- web_pr/static/js/startSnake.js
+++ web_pr/static/js/startSnake.js
@@ -1,15 +1,18 @@
 
 startform= document.getElementById("startform");
+formGuestbook= document.getElementById('guestbook_form');
 
 var csrfToken = getCookie('csrftoken');
 var csrfInput = document.createElement('input');
 csrfInput.type = 'hidden';
 csrfInput.name = 'csrfmiddlewaretoken';
 csrfInput.value = csrfToken;
-startform.appendChild(csrfInput);
+
+
 
 function getCookie(name) {
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   }
+
